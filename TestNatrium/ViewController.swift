76b472c232eccac21ce47527f13dccbee8806bf3
Tanasak Ngerniam <nilc.nolan@gmail.com>
2018-02-26 15:28:39 +0700

    Initial Commit

---
+++ TestNatrium/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestNatrium
+//
+//  Created by Tanasak.Nge on 26/2/2561 BE.
+//  Copyright © 2561 Tanasak.Nge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
